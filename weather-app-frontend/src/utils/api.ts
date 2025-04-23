bb278981af6becf2c48b2d4c25ccbce2ca44e97f
Maunducyrus <maunducyrus123@gmail.com>
2025-04-23 14:36:10 +0300

    api.ts final commit

--- weather-app-frontend/src/utils/api.ts
+++ weather-app-frontend/src/utils/api.ts
@@ -1,6 +1,7 @@
 export const fetchWeatherData = async (city: string) => {
   try {
-    const response = await fetch('http://localhost:8000/api/weather?city=Nairobi');
+    const response = await fetch(`http://127.0.0.1:8000/api/v1/weather?city=${encodeURIComponent(city)}`);
+    // const response = await fetch('http://localhost:8000/api/weather?city=Nairobi');
     if (!response.ok) {
       throw new Error('Failed to fetch weather data');
     }
